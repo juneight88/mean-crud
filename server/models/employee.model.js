94c40578eaf84168679f1a8b05ea556e2c9c2f1f
juneight88 <159888068+juneight88@users.noreply.github.com>
2024-05-15 19:37:59 +0800

    Update employee.model.js

--- server/models/employee.model.js
+++ server/models/employee.model.js
@@ -1,8 +1,5 @@
 const mongoose = require('mongoose')
 
-module.exports = mongoose.model('Employee', {
-    fullName: { type: String },
-    position: { type: String },
-    location: { type: String },
-    salary: { type: Number },
-})
+exports.model (
+    
+)
