edc6b7312d6a02405e923d6c67a3635df226596c
juneight88 <159888068+juneight88@users.noreply.github.com>
2024-05-15 19:20:12 +0800

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,7 +1,4 @@
 const mongoose = require('mongoose')
-
-const dbUri = 'mongodb+srv://jaranillajuneight2:meanstack123@cluster0.ezerpcf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-
 mongoose.set('strictQuery', false)
 
 module.exports = () => {
